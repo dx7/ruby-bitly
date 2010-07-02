4a90fa15125d0a8f54f72e778148921152176ce3
Rafael <rafaeldx7@gmail.com>
2010-07-02 03:25:34 -0300

    test improved

--- spec/ruby-bitly_spec.rb
+++ spec/ruby-bitly_spec.rb
@@ -3,11 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "RubyBitly" do
   
   it "Load personal data from ~/.bitly" do
-    Bitly.load_personal_data.should == { "key" => "R_59c1b174b21d92b2beeb4787a6d7ebaf", "login" => "rafaeldx7" }
+    file_data = Bitly.load_personal_data
+    file_data["key"].should_not be_empty
+    file_data["login"].should_not be_empty
   end
   
   it "Shorten log url staticaly and return a hash" do
-    response = Bitly.post_shorten("http://google.com")
+    response = Bitly.post_shorten "http://google.com"
 
     response["status_code"].should == 200
     response["status_txt"].should == "OK"
@@ -56,10 +58,16 @@ describe "RubyBitly" do
     url.status_txt.should == "OK"
   end
   
+  it "Update object as read only" do
+    url = Bitly.new
+    url.read_only?.should be false
+    url.read_only_now!
+    url.read_only?.should be true
+  end
+  
   it "Shorten url and return an object" do
     url = Bitly.shorten("http://google.com")
     url.should be_an_instance_of(Bitly)
-    url.read_only?.should be true
 
     url.status_code.should == 200
     url.status_txt.should == "OK"
@@ -75,47 +83,38 @@ describe "RubyBitly" do
     url.read_only?.should be false
   end
   
-  it "Old bitly object should be read only" do
-    url = Bitly.new
-    url.long_url = "http://xlii.com.br"
-    url.shorten
+  it "Shortened object should be read only" do
+    url = Bitly.shorten "http://google.com"
     url.read_only?.should be true
   end
   
-  it "Attribute long_url should be updated when object is not read only" do
+  it "Expanded object should be read only" do
+    url = Bitly.expand "http://bit.ly/bcvNe5"
+    url.read_only?.should be true
+  end
+  
+  it "Attribute long_url should be updatable when object is not read only" do
     url = Bitly.new
-    url.read_only?.should be false
     url.long_url = "http://google.com"
     url.long_url.should == "http://google.com"
     
-    url.read_only?.should be false
     url.long_url = "http://xlii.com.br"
     url.long_url.should == "http://xlii.com.br"
   end
   
-  it "Attribute long_url should not be updated when object is read only" do
+  it "Attribute long_url should not be updatable when object is read only" do
     url = Bitly.new
-    url.read_only?.should be false
     url.long_url = "http://google.com"
     url.long_url.should == "http://google.com"
-    url.shorten
+    url.read_only_now!
     
-    url.read_only?.should be true
     url.long_url = "http://xlii.com.br"
     url.long_url.should == "http://google.com"
-  end
-  
-  it "Update object as read only" do
-    url = Bitly.new
-    url.read_only?.should be false
-    url.read_only_now!
-    url.read_only?.should be true
   end
 
   it "Expand url and return an object" do
     url = Bitly.expand "http://bit.ly/bcvNe5"
     url.should be_an_instance_of(Bitly)
-    url.read_only?.should be true
 
     url.long_url.should == "http://google.com"
     url.short_url.should == "http://bit.ly/bcvNe5"
@@ -127,23 +126,19 @@ describe "RubyBitly" do
   
   it "Attribute short_url should be updated when object is not read only" do
     url = Bitly.new
-    url.read_only?.should be false
     url.short_url = "http://bit.ly/bcvNe5"
     url.short_url.should == "http://bit.ly/bcvNe5"
     
-    url.read_only?.should be false
     url.short_url = "http://bit.ly/xlii42"
     url.short_url.should == "http://bit.ly/xlii42"
   end
   
   it "Attribute short_url should not be updated when object is read only" do
     url = Bitly.new
-    url.read_only?.should be false
     url.short_url = "http://bit.ly/xlii42"
     url.short_url.should == "http://bit.ly/xlii42"
-    url.expand
+    url.read_only_now!
     
-    url.read_only?.should be true
     url.short_url = "http://bit.ly/bcvNe5"
     url.short_url.should == "http://bit.ly/xlii42"
   end
