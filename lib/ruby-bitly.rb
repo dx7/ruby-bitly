6ef1d8d7af26b0b573a630056fc506de6f8dba1a
Rafael <rafaeldx7@gmail.com>
2010-07-02 23:05:04 -0300

    add clicks

--- lib/ruby-bitly.rb
+++ lib/ruby-bitly.rb
@@ -5,11 +5,11 @@ require 'json'
 
 class Bitly
   
-  attr_reader :response, :status_code, :status_txt, :new_hash, :global_hash, :user_hash, :hash, :response, :long_url, :short_url
+  attr_reader :response, :status_code, :status_txt, :new_hash, :global_hash, :user_hash, :hash, :response, :long_url, :short_url, :global_clicks, :user_clicks
   
   PERSONAL_FILE_PATH = "#{ENV['HOME']}/.bitly"
   REST_API_URL = "http://api.bit.ly"
-  ACTION_PATH = { :shorten => '/v3/shorten', :expand => '/v3/expand' }
+  ACTION_PATH = { :shorten => '/v3/shorten', :expand => '/v3/expand', :clicks => '/v3/clicks' }
   
   def initialize
     @read_only = false
@@ -59,9 +59,23 @@ class Bitly
   
   def expand
     @response = Bitly.post_expand(@short_url)
+    
     @long_url = @response["data"]["expand"].first["long_url"]
     @global_hash = @response["data"]["expand"].first["global_hash"]
     @user_hash = @response["data"]["expand"].first["user_hash"]
+    @status_code = @response["status_code"]
+    @status_txt = @response["status_txt"]
+
+    read_only_now!
+  end
+  
+  def clicks
+    @response = Bitly.get_clicks(@short_url)
+    
+    @global_clicks = @response["data"]["clicks"].first["global_clicks"]
+    @user_clicks = @response["data"]["clicks"].first["user_clicks"]
+    @global_hash = @response["data"]["clicks"].first["global_hash"]
+    @user_hash = @response["data"]["clicks"].first["user_hash"]
     @status_code = @response["status_code"]
     @status_txt = @response["status_txt"]
 
@@ -80,6 +94,12 @@ class Bitly
     JSON.parse(response)
   end
   
+  def Bitly.get_clicks(new_short_url)
+    Bitly.load_personal_data
+    response = RestClient.get("#{REST_API_URL}#{ACTION_PATH[:clicks]}?login=#{@@login}&apiKey=#{@@key}&shortUrl=#{new_short_url}")
+    JSON.parse(response)
+  end
+    
   def Bitly.shorten(new_long_url)
     bitly = Bitly.new
     bitly.long_url = new_long_url
@@ -91,6 +111,13 @@ class Bitly
     bitly = Bitly.new
     bitly.short_url = new_short_url
     bitly.expand
+    bitly
+  end
+  
+  def Bitly.clicks(new_short_url)
+    bitly = Bitly.new
+    bitly.short_url = new_short_url
+    bitly.clicks
     bitly
   end
 end
