91eaeb3200c8ee6629f878e8337103e5ba3d6524
rafaeldx7 <rafaeldx7@gmail.com>
2013-03-08 18:15:33 -0300

    model refat.

--- lib/ruby-bitly.rb
+++ lib/ruby-bitly.rb
@@ -6,44 +6,44 @@ require 'ostruct'
 
 class Bitly < OpenStruct
 
-  class << self
-    attr_accessor :login, :key
-  end
-
   VERSION = '0.1.5'
   REST_API_URL = "http://api.bit.ly"
   ACTION_PATH = { :shorten => '/v3/shorten', :expand => '/v3/expand', :clicks => '/v3/clicks' }
 
-  def Bitly.shorten(new_long_url, login = self.login, key = self.key)
-    response = JSON.parse RestClient.post(REST_API_URL + ACTION_PATH[:shorten], { :longURL => new_long_url, :login => login, :apiKey => key })
+  class << self
+    attr_accessor :login, :key
 
-    bitly = Bitly.new response["data"]
+    def shorten(new_long_url, login = self.login, key = self.key)
+      response = JSON.parse RestClient.post(REST_API_URL + ACTION_PATH[:shorten], { :longURL => new_long_url, :login => login, :apiKey => key })
 
-    bitly.hash_path = response["data"]["hash"] if response["status_code"] == 200
-    bitly.status_code = response["status_code"]
-    bitly.status_txt = response["status_txt"]
+      bitly = new response["data"]
 
-    bitly
-  end
+      bitly.hash_path = response["data"]["hash"] if response["status_code"] == 200
+      bitly.status_code = response["status_code"]
+      bitly.status_txt = response["status_txt"]
 
-  def Bitly.expand(new_short_url, login = self.login, key = self.key)
-    response = JSON.parse RestClient.post(REST_API_URL + ACTION_PATH[:expand], { :shortURL => new_short_url, :login => login, :apiKey => key })
+      bitly
+    end
 
-    bitly = Bitly.new(response["data"]["expand"].first)
-    bitly.status_code = response["status_code"]
-    bitly.status_txt = response["status_txt"]
-    bitly.long_url = bitly.error if bitly.error
+    def expand(new_short_url, login = self.login, key = self.key)
+      response = JSON.parse RestClient.post(REST_API_URL + ACTION_PATH[:expand], { :shortURL => new_short_url, :login => login, :apiKey => key })
 
-    bitly
-  end
+      bitly = new(response["data"]["expand"].first)
+      bitly.status_code = response["status_code"]
+      bitly.status_txt = response["status_txt"]
+      bitly.long_url = bitly.error if bitly.error
 
-  def Bitly.get_clicks(new_short_url, login = self.login, key = self.key)
-    response = JSON.parse RestClient.get("#{REST_API_URL}#{ACTION_PATH[:clicks]}?login=#{login}&apiKey=#{key}&shortUrl=#{new_short_url}")
+      bitly
+    end
 
-    bitly = Bitly.new(response["data"]["clicks"].first)
-    bitly.status_code = response["status_code"]
-    bitly.status_txt = response["status_txt"]
+    def get_clicks(new_short_url, login = self.login, key = self.key)
+      response = JSON.parse RestClient.get("#{REST_API_URL}#{ACTION_PATH[:clicks]}?login=#{login}&apiKey=#{key}&shortUrl=#{new_short_url}")
 
-    bitly
+      bitly = new(response["data"]["clicks"].first)
+      bitly.status_code = response["status_code"]
+      bitly.status_txt = response["status_txt"]
+
+      bitly
+    end
   end
 end
